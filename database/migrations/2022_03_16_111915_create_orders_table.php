b4d3183b88c6544d84eefd64ba8d8ae634866d86
BDevPro9707 <behruzmaxmudovich9798@gmail.com>
2022-03-16 18:35:12 +0500

    order and order_details CRUD

---
+++ database/migrations/2022_03_16_111915_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('all_price');
+            $table->boolean('status');
+            $table->integer('quantity');
+            $table->date('order_date');
+            $table->date('close_date');
+            $table->string('payment_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
