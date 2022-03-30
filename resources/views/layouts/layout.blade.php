1e544daa371c4d0ee78b6b846d549fbce191f308
norkulov2oo1 <norqulovramazon9@gmail.com>
2022-03-30 10:56:06 +0500

    changes in layout.blade

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -31,7 +31,7 @@
                             <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
                             <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
                             <li><a href="cart.html"><i class="fa fa-user"></i> My Cart</a></li>
-                            <li><a href="checkout.html"><i class="fa fa-user"></i> Checkout</a></li>
+                            <li><a href="{{route('checkout')}}"><i class="fa fa-user"></i> Checkout</a></li>
                             <li><a href="#"><i class="fa fa-user"></i> Login</a></li>
                         </ul>
                     </div>
@@ -69,7 +69,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="./"><img src="{{asset('assets/img/logo.png')}}"></a></h1>
+                        <h1><a href="{{route('home')}}"><img src="{{asset('assets/img/logo.png')}}"></a></h1>
                     </div>
                 </div>
 
