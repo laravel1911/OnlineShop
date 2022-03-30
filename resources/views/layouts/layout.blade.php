3a5ba0276d3ba10c2d645cea33277738d5d42670
norkulov2oo1 <norqulovramazon9@gmail.com>
2022-03-30 11:02:29 +0500

    changes in layout.blade

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -28,7 +28,7 @@
                 <div class="col-md-8">
                     <div class="user-menu">
                         <ul>
-                            <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
+                            <li><a href="{{route('dashboard')}}"><i class="fa fa-user"></i> My Account</a></li>
                             <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
                             <li><a href="cart.html"><i class="fa fa-user"></i> My Cart</a></li>
                             <li><a href="{{route('checkout')}}"><i class="fa fa-user"></i> Checkout</a></li>
