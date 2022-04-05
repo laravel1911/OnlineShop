3852703bf97de059ffeb501de3ea740503af763a
norkulov2oo1 <norqulovramazon9@gmail.com>
2022-04-05 22:16:22 +0500

    add column to orders table changes in migration

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -31,7 +31,7 @@
                         <ul>
                             <li><a href="{{route('dashboard')}}"><i class="fa fa-user"></i> My Account</a></li>
                             <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
-                            <li><a href="{{route('cart')}}"><i class="fa fa-user"></i> My Cart</a></li>
+                            <li><a href="{{route('actioncart')}}"><i class="fa fa-user"></i> My Cart</a></li>
                             <li><a href="{{route('checkout')}}"><i class="fa fa-user"></i> Checkout</a></li>
                             <li><a href="{{route('login')}}"><i class="fa fa-user"></i> Login</a></li>
                         </ul>
